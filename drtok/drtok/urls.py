4f1867e66b7d232624540a20c3008069444909df
Marina Krivcun <marinaktsn@gmail.com>
2021-08-11 15:41:04 +0300

    views and urls updated

--- drtok/drtok/urls.py
+++ drtok/drtok/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_simplejwt import views as jwt_views
-from .usrt import views
+# from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
+from .usrt import admin
+from .usrt import views as accounts
 
 # rest_framework automatic URL routing.
 # router = routers.DefaultRouter()
@@ -24,12 +26,19 @@ from .usrt import views
 
 # include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/login/', accounts.customer_login, name='login'),
+    path('accounts/logout/', accounts.BlacklistTokenView, name='logout'),
+    path('accounts/profile/',accounts.profile),
+    path('accounts/register/', accounts.register, name = 'register'),
+    path('token/refresh/', TokenRefreshView.as_view(),name="token_refresh"),
+    path('token/verify/', TokenVerifyView.as_view(),name="token_verify"),
     # path('', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/users/', views.ListUsers.as_view()),
-    path('', views.UserAPI.as_view()),
-    path('<int:pk>', views.UserRetrieveUpdateDestroyAPI.as_view()),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/users/', views.ListUsers.as_view()),
+    # path('', views.UserAPI.as_view()),
+    # path('<int:pk>', views.UserRetrieveUpdateDestroyAPI.as_view()),
+    # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
--- drtok/drtok/usrt/views.py
+++ drtok/drtok/usrt/views.py
@@ -1,67 +1,173 @@
-from django.contrib.auth.models import User
-# from rest_framework import viewsets
-from rest_framework import permissions
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from rest_framework import status
-from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-# from rest_framework.views import APIView
-from . import serializers
-# from .serializers import UserSerializer
-from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .serializers import ReadOnlyUserSerializer, WriteOnlyUserSerializer
 
-class ListUsers(ListAPIView):
-    """
-    View to list all users in the system.
-    * Only admin users are able to access this view.
-    """
-    # authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAdminUser]
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = serializers.ReadOnlyUserSerializer
+@api_view(['GET','POST'])
+def register(request):
+    if request.method == 'POST':
+        serializer=WriteOnlyUserSerializer(data=request.data)
+        if serializer.is_valid():
+            username = request.data['username']
+            first_name = request.data['first_name']
+            last_name = request.data['last_name']
+            is_active = request.data['is_active']
+            password = request.data['password']
+            password2 = request.data['confirm']
+            if password == password2:
+                if User.objects.filter(username=username).exists():
+                    return Response('User with same username already exists',status=status.HTTP_403_FORBIDDEN)
+                else:
+                    user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, is_active=is_active)
+                    user.save()
+                    return Response('User Registered successfully',status=status.HTTP_201_CREATED)
+            else:
+                return Response('Password and Confirm Password do not match',status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(f"Welcome.")
 
-    # def get(self, request, format=None):
-    #     """
-    #     Return a list of all users.
-    #     """
-    #     usernames = [user.username for user in User.objects.all()]
-    #     return Response(usernames)
+@api_view(['GET','PATCH','DELETE'])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    try:
+        user = User.objects.get(id=request.user.id)
+        if request.method == 'GET':
+            serializer = ReadOnlyUserSerializer(user)
+            return Response(serializer.data,status=status.HTTP_200_OK)
 
-class UserAPI(ListCreateAPIView):
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = serializers.WriteOnlyUserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+        if request.method == 'PATCH':
+            serializer=WriteOnlyUserSerializer(data=request.data)
+            if serializer.is_valid():
+                user.username = request.data['username']
+                user.first_name = request.data['first_name']
+                user.last_name = request.data['last_name']
+                user.is_active = request.data['is_active']
+                user.password = request.data['password']
+                user.save()
+                return Response('User updated successfully',status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+        if request.method == 'DELETE':
+            serializer=WriteOnlyUserSerializer(data=request.data)
+            if serializer.is_valid():
+                user.is_active = False
+                user.save()
+                return Response('User deleted successfully',status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response({"status": True,
-                         "message": "User added ",
-                         "data": serializer.data},
-                        status=status.HTTP_201_CREATED, headers=headers)
+@api_view(['GET','POST'])
+def login(request):
+    if request.method == 'GET':  
+        return Response('WELCOME. Enter your login credentials(username and password)')
+    else:    
+        username = request.data['username']
+        password = request.data['password']
+        
+        if User.objects.filter(username=username).exists()==False:
+            return Response('User does not exist',status=status.HTTP_404_NOT_FOUND)        
+        else:
+            user = authenticate(username=username, password=password) 
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                },status=status.HTTP_200_OK)
+            else:                       
+                return Response('Username or Password is incorrect',status=status.HTTP_401_UNAUTHORIZED)   
+
+@api_view(['GET','POST'])
+@permission_classes([AllowAny])
+def BlacklistTokenView(request):
+    if request.method=='POST':
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response('Logged Out',status=status.HTTP_200_OK)
+        except Exception as e:
+            print(Exception)
+            return Response('Exception',status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# from django.contrib.auth.models import User
+# # from rest_framework import viewsets
+# from rest_framework import permissions
+# from rest_framework import status
+# from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework.response import Response
+# # from rest_framework.views import APIView
+# from . import serializers
+# # from .serializers import UserSerializer
+# from django.contrib.auth.models import User
+
+# class ListUsers(ListAPIView):
+#     """
+#     View to list all users in the system.
+#     * Only admin users are able to access this view.
+#     """
+#     # authentication_classes = [authentication.TokenAuthentication]
+#     permission_classes = [permissions.IsAdminUser]
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = serializers.ReadOnlyUserSerializer
+
+#     # def get(self, request, format=None):
+#     #     """
+#     #     Return a list of all users.
+#     #     """
+#     #     usernames = [user.username for user in User.objects.all()]
+#     #     return Response(usernames)
+
+# class UserAPI(ListCreateAPIView):
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = serializers.WriteOnlyUserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_create(serializer)
+#         headers = self.get_success_headers(serializer.data)
+#         return Response({"status": True,
+#                          "message": "User added ",
+#                          "data": serializer.data},
+#                         status=status.HTTP_201_CREATED, headers=headers)
 
 
-class UserRetrieveUpdateDestroyAPI(RetrieveUpdateDestroyAPIView):
-    serializer_class = serializers.WriteOnlyUserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+# class UserRetrieveUpdateDestroyAPI(RetrieveUpdateDestroyAPIView):
+#     serializer_class = serializers.WriteOnlyUserSerializer
+#     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        return User.objects.filter(id=self.kwargs.get('pk', None))
+#     def get_queryset(self):
+#         return User.objects.filter(id=self.kwargs.get('pk', None))
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+#     def update(self, request, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance, data=request.data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_update(serializer)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, 
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
+#         if getattr(instance, '_prefetched_objects_cache', None):
+#             # If 'prefetch_related' has been applied to a queryset, 
+#             # forcibly invalidate the prefetch cache on the instance.
+#             instance._prefetched_objects_cache = {}
 
-        return Response({"status": True, "message": "User updated ", "data": serializer.data})
+#         return Response({"status": True, "message": "User updated ", "data": serializer.data})
 
 
 
